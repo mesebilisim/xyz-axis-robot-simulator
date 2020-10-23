86fd09a08951874edfbe9b865039e37a543dffab
Doğukan Düğün <54134874+DogukanDugun@users.noreply.github.com>
2020-10-23 15:07:43 +0300

    Update xyz_treed_cartesian_simulator.py

--- xyz_treed_cartesian_simulator.py
+++ xyz_treed_cartesian_simulator.py
@@ -1,3 +1,8 @@
+# 23.10.2020
+# Project Name: XYZ 3D CARTESİAN SİMULATOR
+# Project Destination : G-CODE PARSER
+# Author : DOGUKAN DUGUN
+
 
 from tkinter.filedialog import *
 
@@ -9,51 +14,54 @@ import time
 import threading
 
 from matplotlib.figure import Figure
-yaza = ""
 
-def dosyasec():
-    dosyaal=askopenfilename()
-    global yaza
-    yaza = dosyaal
-    btdosyayaz.invoke()
-    if dosyaal:
-        yazi2.config(text=dosyaal)
+text1 = ""
+
+
+def SelectFile():
+    file1 = askopenfilename()
+    global text1
+    text1 = file1
+    btwritefile.invoke()
+    if file1:
+        text2.config(text=file1)
 
     else:
-        yazi2.config(text="File Not Selected")
+        text2.config(text="File Not Selected")
 
-def dosyayaz():
-    satirsayisi = 0
-    gcode = open(yaza, "r", encoding="utf-8")
+
+def WriteFile():
+    linenumber = 0
+    gcode = open(text1, "r", encoding="utf-8")
 
     for i in gcode:
-        satirsayisi += 1
+        linenumber += 1
 
-    gcode = open(yaza, "r", encoding="utf-8")
-    k = 0
+    gcode = open(text1, "r", encoding="utf-8")
+    kline = 0
     i = 0
-    s = 0
-    sayac1 = 0
+
+
 
     global corx
-    corx =[]
+    corx = []
     global cory
     cory = []
     global corz
     corz = []
 
-    while k < satirsayisi:
+    while kline < linenumber:
 
-        k += 1
-        print(str(k) + ". satıra girdi-----------")
+        kline += 1
+        # print(str(kline) + ". satıra girdi-----------")
         global z
         z = gcode.readline()
 
         # print(z, end="")
         while i < len(z) + 1:
 
-            print(str(k) + ".satırda")
-            print(str(i) + ".indexe baktı")
+            # print(str(kline) + ".satırda")
+            # print(str(i) + ".indexe baktı")
 
             if z[i] == ";" or i == len(z) - 1:
 
@@ -61,52 +69,48 @@ def dosyayaz():
                 break
 
             elif z[i] == "Z":
-                sayac2 = 0
+                counter2 = 0
                 s = i
 
                 while z[s] != " " and i < len(z) - 1:
-
                     i += 1
-                    sayac2 += 1
+                    counter2 += 1
                     s += 1
-                i = i - sayac2
+                i = i - counter2
 
-                corz.append(z[i + 1:i + sayac2])
+                corz.append(z[i + 1:i + counter2])
             elif z[i] == "X":
-                sayac2 = 0
+                counter2 = 0
 
                 s = i
 
                 while z[s] != " ":
-                    sayac2 += 1
+                    counter2 += 1
                     s = s + 1
 
-                corx.append(z[i + 1:i + sayac2])
+                corx.append(z[i + 1:i + counter2])
 
             elif z[i] == "Y":
-                sayac2 = 0
+                counter2 = 0
                 s = i
-
 
                 while z[s] != " " and i < len(z) - 2:
                     i += 1
-                    sayac2 += 1
+                    counter2 += 1
                     s += 1
-                i = i - sayac2
+                i = i - counter2
 
-                cory.append(z[i + 1:i + sayac2])
+                cory.append(z[i + 1:i + counter2])
 
             i += 1
-
-
 
     for ii in range(0, len(corx)):
         corx[ii] = float(corx[ii])
     for ii in range(0, len(cory)):
-        try :
+        try:
             cory[ii] = float(cory[ii])
         except ValueError:
-            cory[ii] = cory[ii-1]
+            cory[ii] = cory[ii - 1]
 
     for ii in range(0, len(corz)):
         corz[ii] = float(corz[ii])
@@ -118,12 +122,12 @@ def dosyayaz():
 
     global corz_iter
     corz_iter = iter(corz)
-    print("VAR OLAN TÜM X KORDİNATLARI :", end="")
-    print(corx)
-    print("VAR OLAN TÜM Y KORDİNATLARI :", end="")
-    print(cory)
-    print("VAR OLAN TÜM Z KODLARI :", end="")
-    print(corz)
+    # print("VAR OLAN TÜM X KORDİNATLARI :", end="")
+    # print(corx)
+    # print("VAR OLAN TÜM Y KORDİNATLARI :", end="")
+    # print(cory)
+    # print("VAR OLAN TÜM Z KODLARI :", end="")
+    # print(corz)
 
 
 # --------------------------------------------------------------------------------------------------------------
@@ -174,56 +178,58 @@ a.scatter(0, -235, 0, c="g")
 a.scatter(0, 235, 0, c="g")
 
 
-def calistir():
-    tt = threading.Thread(target=calisartik)
+def run1():
+    tt = threading.Thread(target=run2)
     tt.start()
-def calisartik ():
-    sayac4 = 0
+
+
+def run2():
+    counter4 = 0
 
     global g
-    global speed2,speed3
-    speed2 = giris1z.get()
+    global speed2, speed3
+    speed2 = entry1.get()
     speed3 = float(speed2)
     global gcode1
-    gcode1 = open(yaza, "r", encoding="utf-8")
-    while sayac4 < len(corx) and sayac4 < len(cory) and sayac4 < len(corz):
+    gcode1 = open(text1, "r", encoding="utf-8")
+    while counter4 < len(corx) and counter4 < len(cory) and counter4 < len(corz):
         # print(type(speed3))
         time.sleep(speed3)
-        g=gcode1.readline()
-        listbox.insert(END,g)
+        g = gcode1.readline()
+        listbox.insert(END, g)
         listbox.selection_clear(0, END)
         listbox.selection_set(listbox.size() - 1, listbox.size() - 1)
         listbox.see(END)
-        sayac4 += 1
-        goster1.invoke()
+        counter4 += 1
+        point1.invoke()
 
-def noktabirgoster():
 
-    kordinatx=int(axisentryx.get())
-    kordinaty=int(axisentryy.get())
-    kordinatz=int(axisentryz.get())
-    kordinatxeksi=-(kordinatx)
-    kordinatyeksi = -(kordinaty)
-    kordinatzeksi = -(kordinatz)
+def run3():
+    coordinatex = int(axisentryx.get())
+    coordinatey = int(axisentryy.get())
+    coordinatez = int(axisentryz.get())
+    coordinatexeksi = -(coordinatex)
+    coordinateyeksi = -(coordinatey)
+    coordinatezeksi = -(coordinatez)
 
-    x = [kordinatx, kordinatxeksi]
-    y = [kordinatxeksi, kordinatxeksi]
-    z = [kordinatxeksi, kordinatxeksi]
+    x = [coordinatex, coordinatexeksi]
+    y = [coordinatexeksi, coordinatexeksi]
+    z = [coordinatexeksi, coordinatexeksi]
 
-    x1 = [kordinatxeksi, kordinatx]
-    y1 = [kordinatx, kordinatx]
-    z1 = [kordinatxeksi, kordinatxeksi]
+    x1 = [coordinatexeksi, coordinatex]
+    y1 = [coordinatex, coordinatex]
+    z1 = [coordinatexeksi, coordinatexeksi]
 
     x2 = [0, 0]
-    y2 = [kordinatx, kordinatx]
-    z2 = [kordinatxeksi,kordinatx]
+    y2 = [coordinatex, coordinatex]
+    z2 = [coordinatexeksi, coordinatex]
 
     x3 = [0, 0]
-    y3 = [kordinatxeksi, kordinatxeksi]
-    z3 = [kordinatxeksi, kordinatx]
+    y3 = [coordinatexeksi, coordinatexeksi]
+    z3 = [coordinatexeksi, coordinatex]
 
     x4 = [0, 0]
-    y4 = [kordinatx, kordinatxeksi]
+    y4 = [coordinatex, coordinatexeksi]
     z4 = [0, 0]
 
     bas1 = next(corx_iter)
@@ -236,9 +242,9 @@ def noktabirgoster():
     a = fig.add_subplot(projection='3d')
     t = 1
     a.plot(5, 5)
-    a.set_xlabel("x koordinatı")
-    a.set_ylabel("y koordinatı")
-    a.set_zlabel("z koordinatı")
+    a.set_xlabel("coordinate x")
+    a.set_ylabel("coordinate y")
+    a.set_zlabel("coordinate z")
 
     x2 = []
     x2.append(bas1)
@@ -260,65 +266,60 @@ def noktabirgoster():
     a.plot3D(x4, y4, z4)
 
     a.scatter(bas1, bas2, bas3, c="r", s=250)
-    a.scatter(bas1, kordinatxeksi, kordinatxeksi, c="b")
-    a.scatter(bas1, kordinatx, kordinatxeksi, c="b")
-    a.scatter(bas1, kordinatxeksi, bas3, c="g")
-    a.scatter(bas1, kordinatx, bas3, c="g")
+    a.scatter(bas1, coordinatexeksi, coordinatexeksi, c="b")
+    a.scatter(bas1, coordinatex, coordinatexeksi, c="b")
+    a.scatter(bas1, coordinatexeksi, bas3, c="g")
+    a.scatter(bas1, coordinatex, bas3, c="g")
+
 
 speed = 0.3
 label4 = tk.Label(text="SPEED:",
                   font="times 15"
                   )
 label4.place(x=100, y=345)
-giris1z = tk.Entry(form)
-giris1z.insert(0,0.3)
-giris1z.place(x=175, y=350)
-axisentryx=tk.Entry(form)
-axisentryx.place(x=150,y=375)
-axisentryy=tk.Entry(form)
-axisentryy.place(x=300,y=375)
-axisentryz=tk.Entry(form)
-axisentryz.place(x=450,y=375)
-axislabel=tk.Label(text="AXİS WİDTH:",
-                   font="times 15")
-axislabel.place(x=25,y=370)
-axislabel2=tk.Label(text="x",
-                   font="times 15")
+entry1 = tk.Entry(form)
+entry1.insert(0, 0.3)
+entry1.place(x=175, y=350)
+axisentryx = tk.Entry(form)
+axisentryx.place(x=150, y=375)
+axisentryy = tk.Entry(form)
+axisentryy.place(x=300, y=375)
+axisentryz = tk.Entry(form)
+axisentryz.place(x=450, y=375)
+axislabel = tk.Label(text="AXİS WİDTH:",
+                     font="times 15")
+axislabel.place(x=25, y=370)
+axislabel2 = tk.Label(text="x",
+                      font="times 15")
 
-axislabel2.place(x=280,y=370)
-axislabel3=tk.Label(text="x",
-                   font="times 15")
-axislabel3.place(x=430,y=370)
-yazi = tk.Label(text="Plase Select Process File")
-yazi.place(x=175, y=250)
-yazi2 = tk.Label(text="File Not Selected")
-yazi2.place(x=175, y=275)
-goster1 = tk.Button(text="göster", command=noktabirgoster)
-btcalistir = tk.Button(text="RUN",command = calistir)
-btcalistir.place(x=175, y=450)
-btdosyaal = tk.Button(text="Click to select a file",command = dosyasec)
-btdosyaal.place(x=175, y=300)
-btdosyayaz = tk.Button(text="dosya yaz",command = dosyayaz)
-korx=tk.Label(text="COORDİNATE X",
-                   font="times 10")
-korx.place(x=160,y=400)
-kory=tk.Label(text="COORDİNATE Y",
-                   font="times 10")
-kory.place(x=315,y=400)
-korz=tk.Label(text="COORDİNATE Z",
-                   font="times 10")
-korz.place(x=460,y=400)
-listbox = tk.Listbox(form,width=50, height=10)
-listbox.place(x=1300,y=160)
+axislabel2.place(x=280, y=370)
+axislabel3 = tk.Label(text="x",
+                      font="times 15")
+axislabel3.place(x=430, y=370)
+text3 = tk.Label(text="Plase Select Process File")
+text3.place(x=175, y=250)
+text2 = tk.Label(text="File Not Selected")
+text2.place(x=175, y=275)
+point1 = tk.Button(text="göster", command=run3)
+btrun = tk.Button(text="RUN", command=run1)
+btrun.place(x=175, y=450)
+btfile = tk.Button(text="Click to select a file", command=SelectFile)
+btfile.place(x=175, y=300)
+btwritefile = tk.Button(text="dosya yaz", command=WriteFile)
+corx = tk.Label(text="COORDİNATE X",
+                font="times 10")
+corx.place(x=160, y=400)
+cory = tk.Label(text="COORDİNATE Y",
+                font="times 10")
+cory.place(x=315, y=400)
+corz = tk.Label(text="COORDİNATE Z",
+                font="times 10")
+corz.place(x=460, y=400)
+listbox = tk.Listbox(form, width=50, height=10)
+listbox.place(x=1300, y=160)
 scrollbar = Scrollbar(form)
-scrollbar.place(x=1605,y=160,height=320)
+scrollbar.place(x=1605, y=160, height=320)
 listbox.config(yscrollcommand=scrollbar.set)
 scrollbar.config(command=listbox.yview)
 
-
-
-
 form.mainloop()
-
-
-
